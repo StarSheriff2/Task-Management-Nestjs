d7596e792a93daa4a23444ea2fd5e42afdca622a
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-07-21 23:02:07 -0600

    set config file and env files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: [`.env.stage.${process.env.STAGE}`],
-    }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'topSecret51',
