bd33d7a1ba23f62fe1b4cebc1881a6cd51a1d560
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-07-23 23:23:41 -0600

    delete env files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger('AuthController');
   constructor(private authService: AuthService) {}
 
   @Post('/sign-up')
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.logger.verbose(
+      `creating account with username: ${authCredentialsDto.username}`,
+    );
     return this.authService.signUp(authCredentialsDto);
   }
 
